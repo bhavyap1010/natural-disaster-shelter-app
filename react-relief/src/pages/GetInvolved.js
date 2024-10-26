ab3c3860a1f86cfe7c54a6996ae399ade0ad7caf
bhavyap1010 <bhavya.patel9@student.tdsb.on.ca>
2024-10-26 15:15:45 -0400

    removed temp airtable form

--- react-relief/src/pages/GetInvolved.js
+++ react-relief/src/pages/GetInvolved.js
@@ -6,10 +6,6 @@ const GetInvolved = () => {
         <div className="get-involved">
             <h2>Get Involved</h2>
             <p>Learn how you can help your community during disasters.</p>
-
-            <iframe class="airtable-embed" src="https://airtable.com/embed/appZRNWf4GOcgwlPO/pagFfpIWxEbF6I2Lu/form" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
-
-            
         </div>
     );
 };
